cf86ba817477a45c86b1cdc0a37c596d67823e09
dl.pan <dl.pan@hna-tic.com>
2020-02-19 14:34:44 +0800

    mysql与mysql的对比使用md5

--- dbutil/pk.go
+++ dbutil/pk.go
@@ -30,44 +30,33 @@ func GetPKName(db *sql.DB, dbName, tableName string) (string, error) {
 	return fmt.Sprintf("%s", fileds["Column_name"]), nil
 }
 
-//GetTableField 根据配置获取表过滤字段
-func GetTableField(db *sql.DB, dbName, tableName, filter string) ([]string, error) {
-	/*
-		filter_filed="t1,t2" ==> [t1 t2]
-	*/
-	var filedList []string
+//GetTableFieldStr 以“,”拼接返回数据表中的数据
+func GetTableFieldStr(db *sql.DB, dbName, tableName, filter string) (string, error) {
+	/* filter_filed="t1,t2" ==> return "t1","t2" */
 
 	if filter != "" {
-		filedList = strings.Split(filter, ",")
-		return filedList, nil
+		return filter, nil
 	}
+
 	query := fmt.Sprintf("select COLUMN_NAME from `information_schema`.`COLUMNS` where table_schema = \"%s\" and table_name = \"%s\"", dbName, tableName)
 	rows, err := db.Query(query)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	resultList, err := ScanRowToInterfaces(rows)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
+	var filedList []string
 	for _, v := range resultList {
 		t := v.([]byte)
 		filedList = append(filedList, string(t))
 	}
 
-	return filedList, nil
+	return strings.Join(filedList, ","), nil
 }
 
-//GetTableFieldStr 以“,”拼接返回数据
-func GetTableFieldStr(db *sql.DB, dbName, tableName, filter string) (string, error) {
-	filedList, err := GetTableField(db, dbName, tableName, filter)
-	if err != nil {
-		return "", err
-	}
-	filedStr := strings.Join(filedList, ",")
-	return filedStr, nil
-}
 
 //GetTableSchema 只获取表所有字段名称
 func GetTableSchema(db *sql.DB, dbName, tableName, filter string) (string, error) {
@@ -110,4 +99,80 @@ func GetOffsetPk(db *sql.DB, dbName, tableName, pkName string, start, offset int
 	}
 	pk = int(t)
 	return pk, nil
+}
+
+
+// GetTableFieldAndType 返回数据和类型
+func GetTableFieldAndType(db *sql.DB, dbName, tableName, filter string) ([]string, error) {
+	/* 
+	+--------------+-----------+
+	| COLUMN_NAME  | DATA_TYPE |
+	+--------------+-----------+
+	| id           | int       |
+	| title        | varchar   |
+	| created_date | date      |
+	+--------------+-----------+
+	return [id#int title#varchar created_date#date]
+
+	filter_filed="t1,t2" ==> return ["t1#int t2#varchar] 
+	*/
+	query := fmt.Sprintf("select COLUMN_NAME,DATA_TYPE from `information_schema`.`COLUMNS` where table_schema = \"%s\" and table_name = \"%s\"", dbName, tableName)
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	resultMap, err := ScanRowToMapStr(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	var filedList []string
+	if filter != "" {
+		filterList := strings.Split(filter, ",")
+		for _, f := range filterList {
+			filedList = append(filedList, fmt.Sprintf("%s#%s", f, resultMap[f]))
+		}
+	} else {
+		for k, v := range resultMap {
+			filedList = append(filedList, fmt.Sprintf("%s#%s", k, v))
+		}
+	}
+
+	return filedList, nil
+}
+
+
+// FormatCrc 格式化成crc字符串
+func FormatCrc(filedList []string) string {
+	var concatWs []string
+	var concatIsnull []string
+
+	var strType = []string{"char", "varchar"}
+	var bigType = []string{"tinyblob", "tinytext", "blob", "text", "mediumblob", "mediumtext", "longblob", "longtext"}
+	for _, filed := range filedList {
+		cln := strings.Split(filed, "#")
+		concatIsnull = append(concatIsnull, fmt.Sprintf("ISNULL(%s)", cln[0]))
+		if stringInSlice(cln[1], strType) {  // 如果是char、varchar类型，转换为utf8mb4
+			concatWs = append(concatWs, fmt.Sprintf("CONVERT(%s using utf8mb4)", cln[0]))
+		} else if stringInSlice(cln[1], bigType) {  // 如果是blob、text类型，不对内容校验，使用crc32校验
+			concatWs = append(concatWs, fmt.Sprintf("CRC32(%s)", cln[0]))
+		} else {
+			concatWs = append(concatWs, cln[0])
+		}
+	}
+	// 生成crc的核心校验语句
+	f := fmt.Sprintf("COALESCE(LOWER(CONCAT(LPAD(CONV(BIT_XOR(CAST(CONV(SUBSTRING(@crc, 1, 16), 16, 10) AS UNSIGNED)), " +
+			"10, 16), 16, '0'), LPAD(CONV(BIT_XOR(CAST(CONV(SUBSTRING(@crc := md5(CONCAT_WS('#', %s, CONCAT(%s))), " + 
+			"17, 16), 16, 10) AS UNSIGNED)), 10, 16), 16, '0'))),0) AS checksum", strings.Join(concatWs, ","), strings.Join(concatIsnull, ","))
+	return f
+}
+
+// stringInSlice 遍历数组
+func stringInSlice(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
 }
